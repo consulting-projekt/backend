5a8ecb27df192ffc8ae0435f8fe4aab429b5becb
fatal-error-418 <519371@fom-net.de>
2025-05-12 23:20:47 +0200

    "Sie" zu "Du" korrigiert

--- tests/_3_e2e/cases.py
+++ tests/_3_e2e/cases.py
@@ -4,24 +4,24 @@ import pandas as pd
 test_cases = [
     {
         "vars": {
-            "anfrage": "Ich brauche ca. 15:00 einen Bus in die Innenstadt. Gib mir die Route dazu.",
+            "anfrage": "Ich brauche um ca. 15:00 einen Bus in die Innenstadt. Gib mir die Route dazu.",
             # hiermit kann simuliert werden dass systemseitig standort des nutzers verwendet wird
             "start": "Harverdstraße",
-            "answer": "Um <TIME> fährt ein Bus von der Station <START> zur Station <DEST>. Wir wünschen Ihnen eine angenehme Fahrt!"
+            "answer": "Um <TIME> fährt ein Bus von der Station <START> zur Station <DEST>. Wir wünschen dir eine angenehme Fahrt!"
         }
     },
     {
         "vars": {
             "anfrage": "Ich brauche nächste Woche von der Harverdstraße einen Bus in die Innenstadt. Gib mir die Route dazu.",
             "start": None,
-            "answer": "Am <DATE> fährt ein Bus von der Station <START> zur Station <DEST>. Die Abfahrt ist um <TIME>. Wir wünschen Ihnen eine gute Reise!"
+            "answer": "Am <DATE> fährt ein Bus von der Station <START> zur Station <DEST>. Die Abfahrt ist um <TIME>. Wir wünschen dir eine gute Reise!"
         }
     },
     {
         "vars": {
             "anfrage": "Wann kommt der nächste Bus in die Innenstadt?",
             "start": None,
-            "answer": "Könnten Sie uns bitte mitteilen, von wo Sie starten möchten? Sobald wir die Startstation kennen, können wir Ihnen die nächste Verbindung nennen."
+            "answer": "Sag uns bitte, von wo du starten möchtest? Sobald wir die Startstation kennen, können wir dir die nächste Verbindung nennen."
         }
     },
 ]
