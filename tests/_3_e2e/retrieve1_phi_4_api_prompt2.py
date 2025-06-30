a70aa6b0a14b60dde244363bde5892afe98130df
fatal-error-418 <519371@fom-net.de>
2025-07-01 00:22:09 +0200

    _3_e2e finished

--- tests/_3_e2e/retrieve1_phi_4_api_prompt2.py
+++ tests/_3_e2e/retrieve1_phi_4_api_prompt2.py
@@ -20,7 +20,8 @@ MIN_SIMILARITY_SCORE_START = 0.5
 
 def call_api(prompt, options, context):
     anfrage = context.get('vars', {}).get('anfrage', '')
-    params_extract_res = call_gemma3_4b(prompt)
+    #params_extract_res = call_gemma3_4b(prompt) # Prameterextraktion mit Gemma3 deaktiviert
+    params_extract_res = get_answer_json(call_phi_4(prompt))
     params_json = raw_llm2json(params_extract_res)
 
     problems = []
